d2dda88f8f1f569bafdc0092dc210a13dd4cd22d
hossein nick <hossinnick62@gmail.com>
2020-11-04 16:36:40 +0330

    add when method to findByIdTest in SpecialitySDJpaServiceTest

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,6 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,6 +21,19 @@ class SpecialitySDJpaServiceTest {
 
     @InjectMocks
     SpecialitySDJpaService service;
+
+    @Test
+    void findByIdTest() {
+        Speciality speciality= new Speciality();
+
+        when(repository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpeciality=service.findById(1L);
+
+        assertThat(foundSpeciality).isNotNull();
+
+        verify(repository).findById(1L);
+    }
 
     @Test
     void delete() {
