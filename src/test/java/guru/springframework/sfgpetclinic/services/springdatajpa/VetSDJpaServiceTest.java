75269df9fd15627333b856e5536a298f24ed6a9e
hossein nick <hossinnick62@gmail.com>
2020-11-04 16:00:27 +0330

    create VetSDJpaServiceTest with verify method

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService sdJpaService;
+
+    @Test
+    void deleteById() {
+        sdJpaService.deleteById(1L);
+
+        verify(vetRepository,times(1)).deleteById(1L);
+
+        verifyNoMoreInteractions(vetRepository);
+            }
+}
